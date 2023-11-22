661830da03a15c39676bb857a5a2e307c0813830
Adrián Mijarra <adrian.mijarra@gmail.com>
2023-11-22 21:18:12 +0100

    progresando

--- queries.sql
+++ queries.sql
@@ -6,12 +6,14 @@
 SELECT genres, COUNT(*) as total_genres
 FROM movies_metadata
 GROUP BY genres;
+
 /*- Encontrar la película con mayor recaudación*/
 SELECT original_title, revenue
 FROM movies_metadata
 WHERE revenue = (SELECT MAX(revenue) FROM movies_metadata);
-/*- Crear una consulta SQL que cuente el número de películas para adultos*/
-SELECT adult FROM movies_metadata WHERE >= 18
+
+/*- Crear una consulta SQL que cuente el número de películas para adultos NO FUNCIONA SERIA V null OR F*/
+SELECT adult FROM movies_metadata WHERE >= 18 
 
 /*- Calcular la duración promedio de las películas*/
 SELECT AVG(runtime) as averger
@@ -23,17 +25,20 @@ FROM movies_metadata
 WHERE vote_average >= 7.0 
 
 /*- Contar la cantidad de películas por idioma original*/
-SELECT original_title, COUNT(*) as title
+SELECT original_language, COUNT(*) as total
 FROM movies_metadata
-GROUP BY original_lenguages;
+GROUP BY original_language;
 
-/*- Encontrar todas las películas lanzadas en un año específico*/
+/*- Encontrar todas las películas lanzadas en un año específico*/ 
 SELECT *
 FROM movies_metadata
-WHERE EXTRACT(YEAR FROM release_date(release_date, 'YYYY-MM-DD')) = 2020;
+WHERE EXTRACT(YEAR FROM TO_DATE(release_date, 'YYYY-MM-DD')) = 2020;
 
 
 /*- Seleccionar películas que aún no han sido lanzadas (según la columna 'status')*/
-/*- Seleccionar películas que aún no han sido lanzadas (según la columna 'status')*/
+SELECT *
+FROM movies_metadata
+WHERE status = 'Rumored';
+
 /*- Listar las películas que pertenecen a una colección*/
 /*- Calcular la rentabilidad de las películas (suponiendo que los campos 'budget' y 'revenue' están completos y son correctos)*/
