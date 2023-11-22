35f60cda0dbd0c57280a0ab15714a4868ed0005f
Adrián Mijarra <adrian.mijarra@gmail.com>
2023-11-22 20:52:15 +0100

    ultimo falla

--- queries.sql
+++ queries.sql
@@ -11,9 +11,20 @@ SELECT original_title, revenue
 FROM movies_metadata
 WHERE revenue = (SELECT MAX(revenue) FROM movies_metadata);
 /*- Crear una consulta SQL que cuente el número de películas para adultos*/
+SELECT adult FROM movies_metadata WHERE >= 18
+
 /*- Calcular la duración promedio de las películas*/
+SELECT AVG(runtime) as averger
+FROM movies_metadata
+
 /*- Listar películas con una puntuación de audiencia específica o superior*/
+SELECT *
+FROM movies_metadata
+WHERE popularity >= 7.0 
 /*- Contar la cantidad de películas por idioma original*/
+SELECT original_title, COUNT(*) as Titles
+GROUP BY spoken_languages;
+
 /*- Encontrar todas las películas lanzadas en un año específico*/
 /*- Seleccionar películas que aún no han sido lanzadas (según la columna 'status')*/
 /*- Seleccionar películas que aún no han sido lanzadas (según la columna 'status')*/
