fa93d3711a804b5b488c32fcb84dfa696d372740
cgomezlop95 <c.gomezlop95@gmail.com>
2023-11-22 21:24:43 +0100

    adults done

--- queries.sql
+++ queries.sql
@@ -8,13 +8,18 @@ FROM movies_metadata
 WHERE revenue = (SELECT MAX(revenue) FROM movies_metadata);
 
 -- Crear una consulta SQL que cuente el número de películas para adultos
+SELECT original_title, title, adult 
+FROM movies_metadata 
+WHERE adult = true;
 
 -- Calcular la duración promedio de las películas
 SELECT avg(runtime) FROM movies_metadata;
 -- 90.90801576872536|
 
 -- Listar películas con una puntuación de audiencia específica o superior
-SELECT title, vote_average FROM movies_metadata WHERE vote_average > 8;
+SELECT title, vote_average 
+FROM movies_metadata 
+WHERE vote_average > 8;
 
 -- Contar la cantidad de películas por idioma original
 SELECT COUNT(DISTINCT original_language) FROM movies_metadata;
@@ -26,7 +31,9 @@ FROM movies_metadata
 WHERE release_date LIKE '2017%';
 
 -- Seleccionar películas que aún no han sido lanzadas (según la columna 'status')
-SELECT title, STATUS FROM movies_metadata WHERE STATUS = 'Rumored';
+SELECT title, status 
+FROM movies_metadata 
+WHERE status = 'Rumored';
 
 -- Listar las películas que pertenecen a una colección
 SELECT title, belongs_to_collection
@@ -34,5 +41,7 @@ FROM movies_metadata
 WHERE length(belongs_to_collection) > 0;
 
 -- Calcular la rentabilidad de las películas (suponiendo que los campos 'budget' y 'revenue' están completos y son correctos)
-SELECT title, budget, revenue, revenue - budget AS "profability" FROM movies_metadata ORDER BY profability desc;
+SELECT title, budget, revenue, revenue - budget AS "profability" 
+FROM movies_metadata 
+ORDER BY profability desc;
 
